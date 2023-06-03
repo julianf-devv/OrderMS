d082baf4484facf85986717cd2dc0832abc21fec
Julian <julian.hernandezc@outlook.com>
2023-06-03 16:02:44 -0700

    adds javadoc for order flow

--- src/main/java/julianf/dev/anamar/order/OrderService.java
+++ src/main/java/julianf/dev/anamar/order/OrderService.java
@@ -10,14 +10,40 @@ import java.util.List;
 public interface OrderService {
     void save(Orders order);
 
+    /**
+     * Returns a list of all orders without item details.
+     *
+     * @return a list of all orders
+     * @author Julian Fernandez
+     */
     List<OrderNoItemsDTO> findAll();
 
+
+    /**
+     * Returns an order with all its items.
+     *
+     * @return a list of all orders
+     * @author Julian Fernandez
+     */
     OrderDTO getOrder(Long id);
 
     void deleteOrder(Orders order);
 
+    /**
+     * Returns a list of all orders.
+     *
+     * @param items  a list of items to add to the order with their respective quantities and productId
+     * @param id    the id of the order to add the items to
+     * @author Julian Fernandez
+     */
     void addItemToOrder(Long id, List<AddItemToOrderDTO> items);
 
+    /**
+        * Deletes a list of items from an order.
+        * @param orderID the id of the order to delete the items from
+        * @param itemsToDelete a list of items to delete from the order with their respective quantities and productId
+        * @author julian.hernandez
+     */
     void deleteItemFromOrder(Long orderID, List<RemoveItemFromOrderDTO> itemsToDelete);
 
 }
